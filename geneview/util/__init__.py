152003ba922bf6d77f60e44e79a1988972c0b5dd
Shujia Huang <huangshujia9@gmail.com>
2016-01-29 01:14:02 +0100

    add utility package for providing all the utility functions

---
+++ geneview/util/__init__.py
@@ -0,0 +1,12 @@
+"""
+Utility functionality (:mod:`geneview.util`)
+============================================
+
+This package provides general exception/warning definitions used throughout
+geneview, as well as various utility functionality, including plotting and
+unit-testing convenience functions.
+
+"""
+from ._misc import is_numeric
+
+__all__ = ['is_numeric']
---
+++ geneview/util/_misc.py
@@ -0,0 +1,25 @@
+"""
+This module contains miscellaneous functions for ``geneview``.
+"""
+def is_numeric(s):
+    """
+    It's a useful function for checking if a string is a numeric.
+
+    This function could identify any kinds of numeric. e.g. '0.1', '10', '-2.',
+    2.5, etc
+
+    Parameters
+    ----------
+    s : int, float or string.
+        The input could be any kind of single value except the scalable
+        type of python like 'list()', 'dict()', 'set()'
+
+    Notes
+    -----
+        http://stackoverflow.com/questions/354038/how-do-i-check-if-a-string-is-a-number-float-in-python
+    """
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
