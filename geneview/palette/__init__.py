a6c0ad693d8d2b505ca6b7c647a1d94c39734f9f
Shujia Huang <huangshujia9@gmail.com>
2016-01-31 16:35:03 +0100

    rename

--- geneview/palette/__init__.py
+++ geneview/palette/__init__.py
@@ -1,10 +1,10 @@
 from ._xkcd_rgb import xkcd_rgb
 from ._crayons import crayons
 
-from .rcmod import set, reset_defaults, reset_orig, axes_style, set_style, \
+from ._rcmod import set, reset_defaults, reset_orig, axes_style, set_style, \
     plotting_context, set_context, set_palette  
 
-from .palettes import color_palette, hls_palette, husl_palette, mpl_palette, \
+from ._palettes import color_palette, hls_palette, husl_palette, mpl_palette, \
     dark_palette, light_palette, diverging_palette, blend_palette, \
     xkcd_palette, crayon_palette, cubehelix_palette, set_color_codes
 
