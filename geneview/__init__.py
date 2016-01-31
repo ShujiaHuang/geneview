f59479e0acbd024964166bc72b9da55bad1ab541
Shujia Huang <huangshujia9@gmail.com>
2016-01-31 16:28:15 +0100

    define the palette the same with seaborn

--- geneview/__init__.py
+++ geneview/__init__.py
@@ -1,5 +1,11 @@
 from . import gwas
 from . import util
+from .palette import set, reset_defaults, reset_orig, axes_style, set_style, \
+    plotting_context, set_context, set_palette, color_palette, hls_palette, \
+    husl_palette, mpl_palette, dark_palette, light_palette, diverging_palette, \
+    blend_palette, xkcd_palette, crayon_palette, cubehelix_palette, \
+    set_color_codes
+
 from .ext.miscplot import puppyplot, palplot
 
 __version__ = '0.0.1.dev'
