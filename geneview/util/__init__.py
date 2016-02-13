17032c90bb2d47dea11d4566d5f3c5a0898bae3b
Shujia Huang <huangshujia9@gmail.com>
2016-02-14 01:13:23 +0800

    add functions to load a dataset from the online geneview-data repository

--- geneview/util/__init__.py
+++ geneview/util/__init__.py
@@ -10,9 +10,9 @@ unit-testing convenience functions.
 from ._misc import chr_id_cmp, is_numeric, is_integer
 from ._plott import axlabel, despine, offset_spines
 from ._palette import desaturate, set_hls_values, get_color_cycle
-from ._dataset import get_dataset_names
+from ._dataset import get_dataset_names, load_dataset
 
 __all__ = ['chr_id_cmp', 'is_numeric', 'is_integer', 'axlabel', 'despine', 
            'offset_spines', 'desaturate', 'set_hls_values', 'get_color_cycle', 
-           'get_dataset_names',
+           'get_dataset_names', 'load_dataset',
           ]
--- geneview/util/_dataset.py
+++ geneview/util/_dataset.py
@@ -1,7 +1,11 @@
 """
 Utility functions for getting datasets from geneview online dataset repository.
 """
+import os
+import csv
+
 from ..ext.six.moves.urllib.request import urlopen, urlretrieve
+from ._misc import is_numeric
 
 def get_dataset_names():
     """Report available example datasets, useful for reporting issues."""
@@ -13,3 +17,75 @@ def get_dataset_names():
     return [l.text.replace('.csv', '')
             for l in gh_list.find_all("a", {"class": "js-directory-link"})
             if l.text.endswith('.csv')]
+
+
+def load_dataset(name, cache=True, data_home=None):
+    """Load a dataset from the online repository (requires internet).
+
+    Parameters
+    ----------
+    name : str
+        Name of the dataset (`name`.csv on
+        https://github.com/ShujiaHuang/geneview-data).  You can obtain list of
+        available datasets using :func:`get_dataset_names`
+
+    cache : boolean, optional
+        If True, then cache data locally and use the cache on subsequent calls
+
+    data_home : string, optional
+        The directory in which to cache data. By default, uses ~/geneview_data/
+
+
+    Examples
+    --------
+    
+    Load the preview data of GOYA
+
+        >>> import geneview as gv
+        >>> goya_preview = gv.util.load_dataset('GOYA_preview_999lines')
+
+    """
+    path = "https://github.com/ShujiaHuang/geneview-data/raw/master/{0}.csv"
+    full_path = path.format(name)
+
+    if cache:
+        cache_path = os.path.join(_get_data_home(data_home),
+                                  os.path.basename(full_path))
+        if not os.path.exists(cache_path):
+            urlretrieve(full_path, cache_path)
+        full_path = cache_path
+
+    data = []
+    with open(full_path) as f:
+        f_csv = csv.reader(f)
+        headers = next(f_csv)
+        # do not transform the first element of dataset(.csv format)
+        data = [[row[0]] + map(_tr, row[1:]) for row in f_csv] 
+        
+    return data
+
+
+def _tr(s):
+    """ transform numeric to be float data. """
+    return float(s) if is_numeric(s) else s
+
+
+def _get_data_home(data_home=None):
+    """Return the path of the geneview data directory.
+
+    This is used by the ``load_dataset`` function.
+
+    If the ``data_home`` argument is not specified, the default location
+    is ``~/geneview-data``.
+
+    Alternatively, a different default location can be specified using the
+    environment variable ``GENEVIEW_DATA``.
+    """
+    if data_home is None:
+        data_home = os.environ.get('GENEVIEW_DATA',
+                                   os.path.join('~', 'geneview-data'))
+    data_home = os.path.expanduser(data_home)
+    if not os.path.exists(data_home):
+        os.makedirs(data_home)
+
+    return data_home
