591e1fc1d83ab1241c4eebdd64e73ac4d15a8203
Shujia Huang <huangshujia9@gmail.com>
2016-02-14 22:32:46 +0800

    use set_all instead of set (keywords) to be the aesthetics setting function's name

--- geneview/palette/__init__.py
+++ geneview/palette/__init__.py
@@ -4,11 +4,13 @@ http://stanford.edu/~mwaskom/software/seaborn-dev/tutorial/color_palettes.html
 """
 from ._xkcd_rgb import xkcd_rgb
 from ._crayons import crayons
+from ._circos import circos
 
-from ._rcmod import set, reset_default, reset_orig, axes_style, set_style, \
+from ._rcmod import set_all, reset_default, reset_orig, axes_style, set_style, \
     plotting_context, set_context, set_palette  
 
 from ._palettes import color_palette, hls_palette, husl_palette, mpl_palette, \
     dark_palette, light_palette, diverging_palette, blend_palette, \
-    xkcd_palette, crayon_palette, cubehelix_palette, set_color_codes
+    xkcd_palette, crayon_palette, circos_palette, cubehelix_palette, \
+    set_color_codes
 
