e1198ff1aed550811c7b121a4cab277360f6e81a
Shujia Huang <huangshujia9@gmail.com>
2016-02-24 16:24:01 +0800

    add deep bright color palette

--- geneview/palette/_palettes.py
+++ geneview/palette/_palettes.py
@@ -16,20 +16,22 @@ from ._crayons import crayons
 from ._circos import circos
 
 GENEVIEW_PALETTES = dict(
+    dbright=["#056EEE", "#6EEE05", "#EE056E",
+             "#9641E5","#EE8505","#00C2E6"],
     deep=["#4C72B0", "#55A868", "#C44E52",
           "#8172B2", "#CCB974", "#64B5CD"],
     muted=["#4878CF", "#6ACC65", "#D65F5F",
            "#B47CC7", "#C4AD66", "#77BEDB"],
-    pastel=["#92C6FF", "#97F0AA", "#FF9F9A",
-            "#D0BBFF", "#FFFEA3", "#B0E0E6"],
     bright=["#003FFF", "#03ED3A", "#E8000B",
             "#8A2BE2", "#FFC400", "#00D7FF"],
     dark=["#001C7F", "#017517", "#8C0900",
           "#7600A1", "#B8860B", "#006374"],
+    pastel=["#92C6FF", "#97F0AA", "#FF9F9A",
+            "#D0BBFF", "#FFFEA3", "#B0E0E6"],
     colorblind=["#0072B2", "#009E73", "#D55E00",
                 "#CC79A7", "#F0E442", "#56B4E9"],
-    colorful=["#6DC066", "#FD482F", "#8A2BE2", 
-              "#3399FF", "#FE9900", "#00ABA9"]
+    colorful=["#3399FF", "#6DC066", "#FD482F",
+              "#8A2BE2", "#FE9900", "#00ABA9"]
     )
 
 
