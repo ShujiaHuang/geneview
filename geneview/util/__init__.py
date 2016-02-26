edd4c17de8303cfacf126072fae3255ff7e0361c
Shujia Huang <huangshujia9@gmail.com>
2016-02-26 18:07:08 +0800

    add function for importing

--- geneview/util/__init__.py
+++ geneview/util/__init__.py
@@ -8,10 +8,11 @@ unit-testing convenience functions.
 
 """
 from ._misc import chr_id_cmp, is_numeric, is_integer, iqr, _kde_support 
-from ._plott import axlabel, despine, offset_spines
 from ._palette import desaturate, set_hls_values, get_color_cycle
 from ._dataset import get_dataset_names, load_dataset
+from ._plott import axlabel, despine, offset_spines, set_hls_values 
 
 __all__ = ['chr_id_cmp', 'is_numeric', 'is_integer', 'iqr', '_kde_support', 
            'axlabel', 'despine', 'offset_spines', 'desaturate', 'set_hls_values', 
-           'get_color_cycle', 'get_dataset_names', 'load_dataset']
+           'get_color_cycle', 'get_dataset_names', 'load_dataset', 
+           'set_hls_values']
