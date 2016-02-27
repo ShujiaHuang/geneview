54da1d221e60267ad0f5019dd406978ceeca54c3
Shujia Huang <huangshujia9@gmail.com>
2016-02-27 16:11:31 +0800

    update

--- geneview/baseplot/__init__.py
+++ geneview/baseplot/__init__.py
@@ -1,2 +1,4 @@
+# Most of the plot functions named start with _sns_ come from the 
+# seaborn repository.
 from ._sns_distribution import distplot, kdeplot, jointplot
 from ._sns_linearmodel import pairplot 
