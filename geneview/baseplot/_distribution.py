5b8ac986d0e14b088839ae4795a338c071512d64
Shujia Huang <huangshujia9@gmail.com>
2021-04-30 11:32:39 +0800

    fix bugs

--- geneview/baseplot/_distribution.py
+++ geneview/baseplot/_distribution.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
 from ..utils import freedman_diaconis_bins
-from ..palette import circos, color_palette, blend_palette 
+from ..palette import circos, color_palette, blend_palette
 
-def hist2d(x, y, ax=None, bins=None, norm_hist=False, norm=LogNorm(),
-           xlabel=None, ylabel=None, cmap=None,
-           **kwargs):
+
+def hist2d(x, y, ax=None, bins=None, norm=LogNorm(), xlabel=None,
+           ylabel=None, cmap=None, **kwargs):
     """
     Make a 2D histogram plot.
 
@@ -21,8 +21,6 @@ def hist2d(x, y, ax=None, bins=None, norm_hist=False, norm=LogNorm(),
     bins : argument for matplotlib hist(), or None, optional
         Specification of hist bins(integer or array-like), or None to use
         Freedman-Diaconis rule.
-    norm_hist : bool, optional
-        If True, the histogram height shows a density rather than a count.
     xlabel: string, or None, optional
         Set the x axis label of the current axis.
     ylabel: string, or None, optional
@@ -42,8 +40,6 @@ def hist2d(x, y, ax=None, bins=None, norm_hist=False, norm=LogNorm(),
     if ax is None:
         ax = plt.gca()
 
-    kwargs.setdefault("normed", norm_hist)
-
     x = x.astype(np.float64)
     y = y.astype(np.float64)
 
@@ -51,15 +47,17 @@ def hist2d(x, y, ax=None, bins=None, norm_hist=False, norm=LogNorm(),
         x_bins = freedman_diaconis_bins(x)
         y_bins = freedman_diaconis_bins(y)
         bins = min(50, int(np.mean([x_bins, y_bins])))
-    
-    if cmap is None:
-        colors = [circos['meth'+str(100-i)] for i in range(101)]
-        cmap = blend_palette(color_palette(colors), as_cmap=True)        
 
-    ax.hist2d(x, y, bins=bins, norm=LogNorm(), cmap=cmap, **kwargs)
+    if cmap is None:
+        colors = [circos["meth" + str(100 - i)] for i in range(101)]
+        cmap = blend_palette(color_palette(colors), as_cmap=True)
+
+    ax.hist2d(x, y, bins=bins, norm=norm, cmap=cmap, **kwargs)
     ax.grid(False)
 
-    if xlabel: ax.set_xlabel(xlabel)
-    if ylabel: ax.set_ylabel(ylabel)
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
 
     return ax
