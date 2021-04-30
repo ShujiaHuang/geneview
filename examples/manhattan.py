b05cee22f8b9a1e475ad553907b442c5891e2f00
Shujia Huang <huangshujia9@gmail.com>
2021-04-30 10:59:41 +0800

    first commit

---
+++ examples/manhattan.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import geneview as gv
+
+xtick = ["chr"+c for c in list(map(str, range(1, 15))) + ["16", "18", "20", "22"]]
+df = gv.utils.load_dataset("gwas")
+gv.manhattanplot(data=df.loc[:,["#CHROM","POS","P"]], 
+                 hline_kws={"linestyle": "--", "lw": 1.3},
+                 xlabel="Chromosome", 
+                 ylabel="-Log10(P-value)",
+                 xticklabel_kws={"rotation": 45},
+                 xtick_label_set = set(xtick))
+#plt.tight_layout()
+#plt.savefig("manhattan.png")
+plt.show()
