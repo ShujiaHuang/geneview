ed7a47e71c487c36e70dbe11ba75febb1121b48a
Shujia Huang <huangshujia9@gmail.com>
2022-05-25 18:56:11 +0800

    set default rcParams

--- geneview/__init__.py
+++ geneview/__init__.py
@@ -1,6 +1,13 @@
-from .palette import *
+import matplotlib
 
+from .palette import *
 from .karyotype import karyoplot
 from .baseplot import venn, generate_petal_labels
 from .gwas import manhattanplot, qqplot, qqnorm
 from .popgene import admixtureplot
+
+
+matplotlib.rcParams['ps.fonttype']       = 42
+matplotlib.rcParams['pdf.fonttype']      = 42
+matplotlib.rcParams['font.sans-serif']   = ["Arial","Lucida Sans","DejaVu Sans","Lucida Grande","Verdana"]
+matplotlib.rcParams['font.family']       = 'sans-serif'
